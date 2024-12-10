6faa4e6eb12eeeb7c7e2744314a635fda814b27a
Sama-Shahvalizadeh <samashvlzd@gmail.com>
2024-12-10 19:58:32 +0330

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "arabic-black":"ArabicDisplayBlack",
+      "arabic-bold":"ArabicDisplayBold",
+      "arabic-heavy":"ArabicDisplayHeavy",
+      "arabic-light":"ArabicDisplayLight",
+      "arabic-medium":"ArabicDisplayMedium",
+      "arabic-semi-bold":"ArabicDisplaySemiBold",
+      "arabic-thin":"ArabicDisplayThin",
+      "arabic-ultra-light":"ArabicDisplayUltraLight",
+      "saba":"Saba",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
