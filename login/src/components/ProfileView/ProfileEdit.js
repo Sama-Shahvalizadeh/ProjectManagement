4c148dbb775393a4e9d341c0fc73d8a87e9707c8
fatemeseyfi <fatemeazs2000@gmail.com>
2024-12-17 19:53:21 +0330

    finish styles

--- login/src/components/ProfileView/ProfileEdit.js
+++ login/src/components/ProfileView/ProfileEdit.js
@@ -13,9 +13,11 @@ import status from "./icons/status.png";
 import hamburger_icon from "./icons/Frame 1000001439.png";
 import search from "./icons/search-normal.png";
 import btn1 from "./icons/Group 33651.png";
+import btn2 from "./icons/Group 33652.png"
 import profile from "./icons/profile dropdown.png";
 import chat from "./icons/chat.png";
-
+import home from "./icons/home-alt (1).png";
+import left from "./icons/chevron-left (1).png";
 
 export default function ProfileEdit ()  {
   return (    <>
@@ -71,48 +73,114 @@ export default function ProfileEdit ()  {
       {/* محتوای اصلی برنامه */}
       <div className={styles.main_content}>
         <div className={styles.breadcrumbs}>
-          <button><img className={styles.breadcrumbs_icon} src="icons/home-alt (1).png" alt="Home" /></button>
-          <img className={styles.breadcrumbs_separator} src="icons/chevron-left (1).png" alt="Separator" />
-          <button className={styles.breadcrumbs_inactive}>حساب کاربری</button>
-          <img className={styles.breadcrumbs_separator} src="icons/chevron-left (1).png" alt="Separator" />
-          <button className={styles.breadcrumbs_active}>اطلاعات شخصی</button>
+         
+          <button>
+                <img class={styles.breadcrumbs_icon} src={home} alt="icon"/>
+          </button>
+
+          <img class={styles.breadcrumbs_separator} src={left} alt="icon"/>
+
+          <button className={styles.breadcrumbs_inactive}>
+            حساب کاربری
+          </button>
+
+          <img class={styles.breadcrumbs_separator} src={left} alt="icon"/>
+
+          <button className={styles.breadcrumbs_active}>
+            اطلاعات شخصی
+          </button>
         </div>
 
         <div className={styles.profile_form}>
+        {/* <!-- عکس پروفایل --> */}
           <div className={styles.profile_image_edit_wrapper}>
-            <img src="icons/0df984f5b0ca214b6596dd91ef682411.jfif" className={styles.editable_profile_image} alt="profile" />
-            <button className={styles.edit_image_button}><img src="icons/Group 33652.png" className={styles.edit_image_icon} alt="edit" /></button>
+
+            {/* <!-- عکس پروفایل --> */}
+            <img
+              src={userImg}
+              className={styles.editable_profile_image}
+              alt="profile" />
+
+              <div className={styles.edit_image_button}>
+                <button>
+                  <img src={btn2} className={styles.edit_image_icon} alt="icon"/>
+                </button>
+              </div>
+
           </div>
 
+          {/* <!-- اطلاعات کاربر div --> */}
           <div className={styles.profile_form_fields}>
+
+          {/* <!-- نام کاربر div --> */}
             <div className={styles.form_field_row}>
-              <label htmlFor="first-name" className={styles.form_label}>نام</label>
+
+            {/* <!-- نام --> */}
+              <label className={styles.form_label} for="first-name">
+                نام
+              </label>
+
+              {/* <!--  نام input --> */}
               <input type="text" id="first-name" className={styles.form_input} />
+
             </div>
+
+          {/* <!-- نام خانوادگی div --> */}
             <div className={styles.form_field_row}>
-              <label htmlFor="last-name" className={styles.form_label}>نام خانوادگی</label>
-              <input type="text" id="last-name" className={styles.form_input} />
+
+              {/* <!-- نام خانوادگی --> */}
+              <label class={styles.last_name_label} for="last-name">نام خانوادگی</label>
+    
+              {/* <!-- نام خانوادگی input --> */}
+              <input class={styles.form_input} id="last-name"/>
+                      
             </div>
+    
+            {/* <!-- شماره موبایل div --> */}
             <div className={styles.form_field_row}>
-              <label htmlFor="phone-number" className={styles.form_label}>شماره تلفن همراه</label>
-              <input type="text" id="phone-number" className={styles.form_input} />
+
+              {/* <!-- شماره تلفن همراه --> */}
+              <label class={styles.phone_label} for="phone-number">شماره تلفن همراه </label>
+    
+              {/* <!-- شماره تلفن همراه  input --> */}
+              <input class={styles.form_input} id="phone-number"/>
             </div>
+    
+            {/* <!-- ایمیل div --> */}
             <div className={styles.form_field_row}>
-              <label htmlFor="email-address" className={styles.form_label}>ایمیل</label>
-              <input type="text" id="email-address" className={styles.form_input} />
+
+              {/* <!-- ایمیل --> */}
+              <label class={styles.email_label} for="email-address">ایمیل</label>
+    
+              {/* <!-- ایمیل  input --> */}
+              <input class={styles.form_input} id="email-address"/>
             </div>
-            <div className={styles.form_field_row}>
-              <label htmlFor="profession" className={styles.form_label}>تخصص</label>
-              <select id="profession" className={styles.form_select}>
-                <option>انتخاب کنید</option>
-                <option>تخصص1</option>
-                <option>تخصص2</option>
-              </select>
-            </div>
-            <div className={styles.form_field_row}>
-              <label htmlFor="explaining" className={styles.form_label}>توضیحات</label>
-              <textarea id="explaining" className={styles.form_textarea}></textarea>
-            </div>
+    
+
+              {/* <!-- تخصص div --> */}
+              <div className={styles.form_field_row}>
+
+                {/* <!-- تخصص --> */}
+                <label class={styles.expert_label} for="profession">تخصص</label>
+
+                {/* <!-- تخصص  select --> */}
+                <select class={styles.expert_select} name="professions" id="profession">
+                  <option>انتخاب کنید</option>
+                  <option>تخصص1</option>
+                  <option>تخصص2</option>
+                </select>
+    
+              </div>
+    
+              {/* <!-- توضیحات div --> */}
+              <div className={styles.form_field_row}>
+
+                {/* <!-- توضیحات --> */}
+                 <label class={styles.explain_label} for="explaining">توضیحات</label>
+
+                 <textarea class={styles.explain_textaera} id="explaining" type="text" rows="5" style={{textَlign: 'right'}}></textarea>
+              </div>
+
           </div>
 
           <div className={styles.form_buttons_wrapper}>
