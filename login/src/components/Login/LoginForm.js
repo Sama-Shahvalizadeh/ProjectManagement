fc4298716876baa27c11ce82451fe8396f730c55
fatemeseyfi <fatemeazs2000@gmail.com>
2024-12-20 14:45:56 +0330

    restore changes

--- login/src/components/Login/LoginForm.js
+++ login/src/components/Login/LoginForm.js
@@ -1,21 +1,17 @@
+
 import React, { useState, useEffect } from 'react';
-import styles from './LoginForm.module.css';
+import styles from './LoginForm.module.css' ;
 import { Link } from 'react-router-dom';
-import eye from "../../icons/eye.png";
+
 
 export default function LoginForm() {
     const [username, setUsername] = useState('');
-    const [passwordVisible, setPasswordVisible] = useState(false);
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState({ username: '', password: '', general: '' });
     const [isForgotPassword, setIsForgotPassword] = useState(false);
     const [isVerificationPage, setIsVerificationPage] = useState(false); // حالت صفحه وارد کردن کد
     const [timer, setTimer] = useState(240); // 4 دقیقه
     const [verificationError, setVerificationError] = useState(false); // خطای کد تایید
-
-    const togglePasswordVisibility = () => {
-        setPasswordVisible(!passwordVisible);
-    };
 
     useEffect(() => {
         let interval;
@@ -26,34 +22,34 @@ export default function LoginForm() {
         }
         return () => clearInterval(interval);
     }, [isVerificationPage, timer]);
-
+ 
     const handleSubmit = (event) => {
         event.preventDefault();
-
+    
         if (isForgotPassword) {
             setIsVerificationPage(true); // تغییر به صفحه کد تایید
             return;
         }
-
+    
         let valid = true;
         const newErrors = { username: '', password: '', general: '' };
-
+    
         if (!username) {
             newErrors.username = 'وارد کردن این فیلد الزامی است.';
             valid = false;
         }
-
+    
         if (!password) {
             newErrors.password = 'وارد کردن این فیلد الزامی است.';
             valid = false;
         }
-
+    
         setErrors(newErrors);
-
+    
         if (valid) {
             const users = require('./users.json').users;
             const user = users.find((u) => u.username === username);
-
+    
             if (user && user.password === password) {
                 alert('ورود با موفقیت انجام شد');
             } else {
@@ -66,108 +62,103 @@ export default function LoginForm() {
         }
     };
 
-    const handleForgetPassClick = (event) => {
+    const handleForgetPassClick = (event) =>{
         event.preventDefault();
-        window.location.href = 'email';
-    };
+        window.location.href = 'email'
+    }
 
     const handleSignupClick = (event) => {
         event.preventDefault(); // جلوگیری از ارسال فرم
         window.location.href = '/signup'; // انتقال به صفحه signup
     };
 
-    return (
-        <div className={styles.div_frame1} style={{ direction: 'rtl' }} onKeyDown={(event) => {
-            if (event.key === 'Enter') {
-                handleSubmit(event);
-            }
-        }}>
-            <div className={styles.div_login_form}>
-                <div className={styles.div_group_content}>
-                    <div className={styles.login_header}>
-                        <div className={styles.enter_account}>
-                            ورود به حساب کاربری
-                        </div>
-                        <div className={styles.welcome_title}>
-                            به سامانه <span>پروژه نگار</span> خوش آمدید.
-                        </div>
-                    </div>
+    const renderLoginForm = () => (
+        <form onSubmit={handleSubmit}>
+            <div className={styles.div_user_info}>
+                <div className={styles.div_welcome}>
+                <p className={styles.para_enter_account}>ورود به حساب کاربری</p>
+                    <p className={styles.para_welcome}>
+                        به سامانه <span className={styles.span_project_name}>پروژه نگار </span> خوش آمدید.
+                    </p>
+                </div>
 
-                    <div className={styles.user_pass}>
-                        <div className={styles.username_field}>
-                            <label className={styles.para_username} htmlFor="username">نام کاربری</label>
-                            <input
-                                type="text"
-                                id="username"
-                                className={`${styles.input_username} ${errors.username ? styles.input_error : ''}`}
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                            />
-                            {errors.username && (
-                                <div className={styles.error_container}>
-                                    <i className="fas fa-exclamation-triangle icon-warning"></i>
-                                    <p className={styles.error_message}>{errors.username}</p>
-                                </div>
-                            )}
-                        </div>
+                <button type="button" onClick={handleSignupClick} className={styles.btn_signup}>
+                ثبت نام کنید
+                </button>
 
-                        <div className={styles.username_field}>
-                            <label className={styles.para_password} htmlFor="password">رمز عبور</label>
-                            <div className={styles.relative} onClick={togglePasswordVisibility}>
-                                <span className={styles.eye_icon}>
-                                    <img src={eye} alt="Toggle Password Visibility" className={styles.eye} />
-                                </span>
-                                <input
-                                    className={`${styles.input_password} ${errors.password ? styles.input_error : ''}`}
-                                    type={passwordVisible ? 'text' : 'password'}
-                                    id="password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </div>
-                            {errors.password && (
-                                <div className={styles.error_container}>
-                                    <i className="fas fa-exclamation-triangle icon-warning"></i>
-                                    <p className={styles.error_message}>{errors.password}</p>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-
-                    <div className={styles.div_rememeber_me}>
-                        <label className={styles.label_remember_me}>
-                            <input type="checkbox" className={styles.checkbox_remember_me} />
-                            <span className={styles.remember_span}></span>
-                            مرا به خاطر بسپار
-                        </label>
-                        <span className={styles.para_forgot_password} onClick={handleForgetPassClick}>
-                            رمز عبور خود را فراموش کرده ام.
-                        </span>
-                    </div>
-
-                    {errors.general && (
-                        <div className={styles.div_general_error}>
-                            <div className={styles.div_verification_error}>
-                                <i className="fas fa-exclamation-triangle icon-warning"></i>
-                                <p className={styles.error_message}>{errors.general}</p>
-                            </div>
+                <p className={styles.para_username}>نام کاربری</p>
+                <div className={styles.input_wrapper}>
+                    <input
+                        className={`${styles.input_username} ${errors.username ? styles.input_error : ''}`}
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    {errors.username && (
+                        <div className={styles.error_container}>
+                            <i className="fas fa-exclamation-triangle icon-warning"></i>
+                            <p className={styles.error_message}>{errors.username}</p>
                         </div>
                     )}
-
-                    <button type="submit" className={styles.btn_login} onClick={handleSubmit}>
-                        ورود
-                    </button>
-
-                    <div className={styles.register}>
-                        حساب کاربری ندارید؟
-                        <button type="button" onClick={handleSignupClick} className={styles.register_btn}>ثبت نام کنید</button>
-                    </div>
+                </div>
+    
+                <p className={styles.para_password}>رمز عبور</p>
+                <div className={styles.input_wrapper}>
+                    <input
+                        className={`${styles.input_password} ${errors.password ? styles.input_error : ''}`}
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {errors.password && (
+                        <div className={styles.error_container}>
+                            <i className="fas fa-exclamation-triangle icon-warning"></i>
+                            <p className={styles.error_message}>{errors.password}</p>
+                        </div>
+                    )}
                 </div>
             </div>
+    
+            <div className={styles.div_rememeber_me}>
+                <input className={styles.checkbox_remember_me} type="checkbox" id="remember" />
+                <label className={styles.label_remember_me} htmlFor="remember">
+                    مرا به خاطر بسپار
+                </label>
+                <p className={styles.para_forgot_password}  onClick={handleForgetPassClick}>
+                    رمز عبور خود را فراموش کرده ام.
+                </p>
+            </div>
+    
+            {errors.general && (
+                <div className={styles.div_general_error}>
+                    <div className={styles.div_verification_error}>
+                        <i className="fas fa-exclamation-triangle icon-warning"></i>
+                        <p className={styles.error_message}>{errors.general}</p>
+                    </div>
+                </div>
+            )}
+    
+            <button type="submit" className={styles.btn_login}>
+                ورود
+   {!errors && <Link to="/email">
+    </Link>}
+            </button>
+        </form>
+    );
 
+
+    return (
+        <div className={styles.div_frame1}>
             <div className={styles.div_project_title}>
-                پروژه نگار
+                <p className={styles.para_title}>پروژه نگار</p>
+            </div>
+
+            <div className={styles.div_login_form}>
+                <div className={styles.div_group_content}>
+                    {renderLoginForm()}
+                </div>
             </div>
         </div>
     );
 }
+
--- login/src/components/VerificationCode/Verify.js
+++ login/src/components/VerificationCode/Verify.js
@@ -1,139 +1,91 @@
 import React, { useState, useEffect } from 'react';
-import styles from './Verify.module.css';
+import styles from './Verify.module.css' ;
 import { Link } from 'react-router-dom';
-import back from "../../icons/Frame 1000001414.png";
 
-export default function Verify() {
+export default function Verify(){
 
-    const [isVerificationPage, setIsVerificationPage] = useState(false); // حالت صفحه وارد کردن کد
-    const [timer, setTimer] = useState(240); // 4 دقیقه
-    const [verificationError, setVerificationError] = useState(false); // خطای کد تایید
+  const [isVerificationPage, setIsVerificationPage] = useState(false); // حالت صفحه وارد کردن کد
+  const [timer, setTimer] = useState(240); // 4 دقیقه
+  const [verificationError, setVerificationError] = useState(false); // خطای کد تایید
 
-    useEffect(() => {
-        let interval;
-        if (isVerificationPage && timer > 0) {
-            interval = setInterval(() => {
-                setTimer((prevTimer) => prevTimer - 1);
-            }, 1000);
-        }
-        return () => clearInterval(interval);
-    }, [isVerificationPage, timer]);
-
-
-    const handleVerificationSubmit = (event) => {
-        event.preventDefault();
-
-        const inputs = Array.from(document.querySelectorAll('.input-code'));
-        const enteredCode = inputs.map((input) => input.value).join('');
-        const correctCode = '123456'; // کد صحیح
-
-        if (enteredCode !== correctCode) {
-            setVerificationError(true);
-            inputs.forEach((input) => (input.className = 'input-code error'));
-        } else {
-            alert('کد تایید با موفقیت تایید شد!');
-            setVerificationError(false);
-        }
-    };
+  useEffect(() => {
+    let interval;
+    if (isVerificationPage && timer > 0) {
+        interval = setInterval(() => {
+            setTimer((prevTimer) => prevTimer - 1);
+        }, 1000);
+    }
+    return () => clearInterval(interval);
+}, [isVerificationPage, timer]);
 
 
+const handleVerificationSubmit = (event) => {
+  event.preventDefault();
 
-    const handleNewPass = (event) => {
+  const inputs = Array.from(document.querySelectorAll('.input-code'));
+  const enteredCode = inputs.map((input) => input.value).join('');
+  const correctCode = '123456'; // کد صحیح
+
+  if (enteredCode !== correctCode) {
+      setVerificationError(true);
+      inputs.forEach((input) => (input.className = 'input-code error'));
+  } else {
+      alert('کد تایید با موفقیت تایید شد!');
+      setVerificationError(false);
+  }
+};
+  
+  const handleNewPass = (event) =>{
         event.preventDefault();
         window.location.href = 'newpass'
 
-    }
-    const renderVerificationPage = () => (
-        <form onSubmit={handleVerificationSubmit}>
-            <div className={styles.div_verification_container}>
-                <div className={styles.div_welcome}>
-                    <p className={styles.para_enter_account}>وارد کردن کد تایید</p>
-                </div>
-
-                <p className={styles.para_verification_code}>
-                    لطفاً کد تأییدی که به ایمیل
-                    ارسال شد را وارد نمایید.
-                </p>
-
-                <div className={styles.input_code_container}>
-                    {Array.from({ length: 6 }).map((_, index) => (
-                        <input key={index} className={styles.input_code} type="text" maxLength="1" />
-                    ))}
-                </div>
-
-                <p className={styles.timer}>زمان باقی‌مانده: {Math.floor(timer / 60)}:{timer % 60 < 10 ? '0' : ''}{timer % 60}</p>
-
-                {verificationError && (
-                    <div className={styles.div_verification_error}>
-                        <i className="fas fa-exclamation-triangle icon-warning"></i>
-                        <p className={styles.error_message}>کد تایید اشتباه است.</p>
-                    </div>
-                )}
-
-                <p className={styles.para_resend}>
-                    آیا هنوز کد تأیید را دریافت نکرده‌اید؟ <span className={styles.resend_link}>ارسال مجدد</span>
-                </p>
-                <button type="submit" className={styles.btn_login} onClick={handleNewPass}>احراز کد تایید</button>
-            </div>
-        </form>
-    );
-
-    const handleBackPage = (event) => {
-        window.location.href = 'email'; // تغییر به صفحه کد تایید
-    }
-    return (
-        <div className={styles.div_frame1} style={{ direction: 'rtl' }}>
-
-
-            <div className={styles.div_main_validation}>
-
-
-                <div className={styles.top_div}>
-                    <button class={styles.rounded_full}>
-                        <img src={back} class={styles.back_btn} alt="icon" onClick={handleBackPage} />
-                    </button>
-                </div>
-
-
-                <div className={styles.div_group_content}>
-                    {/* {renderVerificationPage()} */}
-                    <div className={styles.div_verification_container}>
-                        <div className={styles.div_welcome}>
-                            وارد کردن کد تایید
-                        </div>
-
-                        <p className={styles.para_verification_code}>
-                            لطفاً کد تأییدی که به ایمیل
-                            ارسال شد را وارد نمایید.
-                        </p>
-
-                        <div className={styles.input_code_container}>
-                            {Array.from({ length: 6 }).map((_, index) => (
-                                <input key={index} className={styles.input_code} type="text" maxLength="1" />
-                            ))}
-                        </div>
-
-                        <p className={styles.timer}>زمان باقی‌مانده: {Math.floor(timer / 60)}:{timer % 60 < 10 ? '0' : ''}{timer % 60}</p>
-
-                        {verificationError && (
-                            <div className={styles.div_verification_error}>
-                                <i className="fas fa-exclamation-triangle icon-warning"></i>
-                                <p className={styles.error_message}>کد تایید اشتباه است.</p>
-                            </div>
-                        )}
-
-                        <p className={styles.para_resend}>
-                            آیا هنوز کد تأیید را دریافت نکرده‌اید؟ <span className={styles.resend_link}>ارسال مجدد</span>
-                        </p>
-                        <button type="submit" className={styles.btn_login} onClick={handleNewPass}>احراز کد تایید</button>
-                    </div>
-
-                </div>
+  }
+  const renderVerificationPage = () => (
+    <form onSubmit={handleVerificationSubmit}>
+        <div className={styles.div_verification_container}>
+            <div className={styles.div_welcome}>
+                <p className={styles.para_enter_account}>وارد کردن کد تایید</p>
             </div>
 
-            <div className={styles.div_project_title}>
-                <p className={styles.para_title}>پروژه نگار</p>
+            <p className={styles.para_verification_code}>
+                لطفاً کد تأییدی که به ایمیل 
+                ارسال شد را وارد نمایید.
+            </p>
+
+            <div className={styles.input_code_container}>
+                {Array.from({ length: 6 }).map((_, index) => (
+                    <input key={index} className={styles.input_code} type="text" maxLength="1" />
+                ))}
+            </div>
+
+            <p className={styles.timer}>زمان باقی‌مانده: {Math.floor(timer / 60)}:{timer % 60 < 10 ? '0' : ''}{timer % 60}</p>
+
+            {verificationError && (
+                <div className={styles.div_verification_error}>
+                    <i className="fas fa-exclamation-triangle icon-warning"></i>
+                    <p className={styles.error_message}>کد تایید اشتباه است.</p>
+                </div>
+            )}
+
+            <p className={styles.para_resend}>
+                آیا هنوز کد تأیید را دریافت نکرده‌اید؟ <span className={styles.resend_link}>ارسال مجدد</span>
+            </p>
+            <button type="submit" className={styles.btn_login} onClick={handleNewPass}>احراز کد تایید</button>
+        </div>
+    </form>
+);
+
+  return (
+    <div className={styles.div_frame1}>
+        <div className={styles.div_project_title}>
+            <p className={styles.para_title}>پروژه نگار</p>
+        </div>
+
+        <div className={styles.div_login_form}>
+            <div className={styles.div_group_content}>
+                {renderVerificationPage()}
             </div>
         </div>
-    );
+    </div>
+);
 }
