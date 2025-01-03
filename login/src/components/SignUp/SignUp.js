7918ca1099d88198f95ddcc0f15ac9bb57082533
ElhamJafari <elhamjafari200312@gmail.com>
2025-01-03 18:22:52 +0330

    Add API to new SignUP

--- login/src/components/SignUp/SignUp.js
+++ login/src/components/SignUp/SignUp.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
+import eye from "../../icons/eye.png";
 import styles from "./signup.module.css";
-import {signUp} from "../../Services/SignUpAPI";  					// استفاده از api
-
+import { signUp } from '../../Services/SignUpAPI';                      // استفاده از api
 
 export default function SignUp() {
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({});                             // برای ذخیره پیام خطا
   const [formValues, setFormValues] = useState({
     firstName: "",
     lastName: "",
@@ -26,6 +26,8 @@ export default function SignUp() {
 
   const handleSubmit = async () => {
     const newErrors = {};
+
+    // بررسی خطاها در فرم
     if (!formValues.firstName.trim()) {
       newErrors.firstName = "وارد کردن این فیلد الزامی است.";
     }
@@ -49,21 +51,21 @@ export default function SignUp() {
 
     if (Object.keys(newErrors).length === 0) {
       try {
-        const response = await signUp(formValues); // ارسال داده‌ها به سرور
-        alert("ثبت‌نام با موفقیت انجام شد!");
-        console.log("Server Response:", response);
-        // انتقال به صفحه ورود یا دیگر صفحات
+        // ارسال داده‌ها به سرور برای ثبت‌نام
+        const response = await signUp(formValues);
+        alert("ثبت‌نام با موفقیت انجام شد.");
+        // انتقال به صفحه ورود پس از موفقیت
+        window.location.href = "/login";
       } catch (error) {
-        alert("خطا در ثبت‌نام. لطفاً دوباره تلاش کنید.");
-        console.error("Error during sign up:", error);
+        alert("خطا در ثبت‌نام. لطفا دوباره تلاش کنید.");
       }
     }
   };
 
   const handleLoginPage = (event) => {
     event.preventDefault();
-    window.location.href = "login";
-  };
+    window.location.href = 'login'
+  }
 
   return (
     <>
@@ -71,61 +73,161 @@ export default function SignUp() {
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
         rel="stylesheet"
       />
-      <div className={styles.main_container}>
+      <div className={styles.main_container} >
+
         <div className={styles.left_side}>پروژه نگار</div>
+
         <div className={styles.sign_form}>
-          <div className={styles.form_header}>
-            <div className={styles.form_header_title}>ایجاد حساب کاربری</div>
-            <div className={styles.welcome_message}>
-              به سامانه <span>پروژه نگار</span> خوش آمدید.
+            <div className={styles.form_header}>
+              <div className={styles.form_header_title}>ایجاد حساب کاربری</div>
+              <div className={styles.welcome_message}>
+                به سامانه <span>پروژه نگار</span> خوش آمدید.
+              </div>
             </div>
-          </div>
-          <div className={styles.form_register}>
-            {/* سایر فیلدهای فرم (نام، نام خانوادگی، ایمیل، و غیره) */}
-            <div className={styles.form_password}>
-              <label className={styles.form_label} htmlFor="password">
-                رمز عبور
-              </label>
-              <div className={styles.relative_position}>
-                <span
-                  className={styles.eye_icon}
-                  onClick={togglePasswordVisibility}
-                >
-                  <img
-                    src={eye}
-                    alt="Toggle Password Visibility"
-                    width="20"
-                    height="20"
-                  />
-                </span>
+
+            <div className={styles.form_register}>
+              <div className={styles.form_first_name}>
+                <label className={styles.form_label} htmlFor="firstName">
+                  نام
+                </label>
                 <input
-                  type={passwordVisible ? "text" : "password"}
-                  id="password"
-                  className={`${styles.form_input_password} ${
-                    errors.password ? styles.input_error : ""
-                  }`}
-                  value={formValues.password}
+                  type="text"
+                  id="firstName"
+                  className={`${styles.form_input} ${errors.firstName ? styles.input_error : ""
+                    }`}
+                  value={formValues.firstName}
                   onChange={handleChange}
                 />
+                {errors.firstName && (
+                  <div className={styles.error_container}>
+                    <i className="fas fa-exclamation-triangle icon-warning"></i>
+                    <p className={styles.error_message}>{errors.firstName}</p>
+                  </div>
+                )}
               </div>
-              {errors.password && (
-                <div className={styles.error_container}>
-                  <i className="fas fa-exclamation-triangle icon-warning"></i>
-                  <p className={styles.error_message}>{errors.password}</p>
+
+              <div className={styles.form_last_name}>
+                <label className={styles.form_label} htmlFor="lastName">
+                  نام خانوادگی
+                </label>
+                <input
+                  type="text"
+                  id="lastName"
+                  className={`${styles.form_input} ${errors.lastName ? styles.input_error : ""
+                    }`}
+                  value={formValues.lastName}
+                  onChange={handleChange}
+                />
+                {errors.lastName && (
+                  <div className={styles.error_container}>
+                    <i className="fas fa-exclamation-triangle icon-warning"></i>
+                    <p className={styles.error_message}>{errors.lastName}</p>
+                  </div>
+                )}
+              </div>
+
+              <div className={styles.form_email}>
+                <label className={styles.form_label} htmlFor="email">
+                  ایمیل
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  className={`${styles.form_input} ${errors.email ? styles.input_error : ""
+                    }`}
+                  value={formValues.email}
+                  onChange={handleChange}
+                />
+                {errors.email && (
+                  <div className={styles.error_container}>
+                    <i className="fas fa-exclamation-triangle icon-warning"></i>
+                    <p className={styles.error_message}>{errors.email}</p>
+                  </div>
+                )}
+              </div>
+
+              <div className={styles.form_phone}>
+                <label className={styles.form_label} htmlFor="phone">
+                  شماره تلفن
+                </label>
+                <input
+                  type="text"
+                  id="phone"
+                  className={`${styles.form_input} ${errors.phone ? styles.input_error : ""
+                    }`}
+                  value={formValues.phone}
+                  onChange={handleChange}
+                />
+                {errors.phone && (
+                  <div className={styles.error_container}>
+                    <i className="fas fa-exclamation-triangle icon-warning"></i>
+                    <p className={styles.error_message}>{errors.phone}</p>
+                  </div>
+                )}
+              </div>
+
+              <div className={styles.form_field}>
+                <label className={styles.form_label} htmlFor="profession">
+                  تخصص
+                </label>
+                <select
+                  id="profession"
+                  className={`${styles.form_select} ${errors.profession ? styles.input_error : ""
+                    }`}
+                  value={formValues.profession}
+                  onChange={handleChange}
+                >
+                  <option value="">انتخاب کنید</option>
+                  <option>آپشن اول</option>
+                  <option>آپشن دوم</option>
+                  <option>آپشن سوم</option>
+                  <option>آپشن چهارم</option>
+                </select>
+                {errors.profession && (
+                  <div className={styles.error_container}>
+                    <i className="fas fa-exclamation-triangle icon-warning"></i>
+                    <p className={styles.error_message}>{errors.profession}</p>
+                  </div>
+                )}
+              </div>
+
+              <div className={styles.form_password}>
+                <label className={styles.form_label} htmlFor="password">
+                  رمز عبور
+                </label>
+                <div className={styles.relative_position}>
+                  <span className={styles.eye_icon} onClick={togglePasswordVisibility}>
+                    <img src={eye} alt="Toggle Password Visibility" width="20" height="20" />
+                  </span>
+                  <input
+                    type={passwordVisible ? "text" : "password"}
+                    id="password"
+                    className={`${styles.form_input_password} ${errors.password ? styles.input_error : ""
+                      }`}
+                    value={formValues.password}
+                    onChange={handleChange}
+                  />
                 </div>
-              )}
+                {errors.password && (
+                  <div className={styles.error_container}>
+                    <i className="fas fa-exclamation-triangle icon-warning"></i>
+                    <p className={styles.error_message}>{errors.password}</p>
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
-          <button className={styles.submit_button} onClick={handleSubmit}>
-            ورود
-          </button>
-          <div className={styles.have_account}>
-            از قبل حساب دارید؟
-            <button className={styles.login_link} onClick={handleLoginPage}>
-              وارد حساب خود شوید
+
+            <button className={styles.submit_button} onClick={handleSubmit}>
+              ثبت‌نام
             </button>
-          </div>
+
+            <div className={styles.have_account}>
+              از قبل حساب دارید؟
+              <button className={styles.login_link} onClick={handleLoginPage}>وارد حساب خود شوید</button>
+            </div>
+
         </div>
+
       </div>
     </>
   );
